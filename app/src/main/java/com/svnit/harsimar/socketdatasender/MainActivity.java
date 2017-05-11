9c7648a1369f929f607e034dc79714e669d49a74
Harsimar <simarsingh24@gmail.com>
2017-05-11 19:49:26 +0530

    code improvements

--- app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
@@ -2,6 +2,7 @@ package com.svnit.harsimar.socketdatasender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,22 +10,31 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String serverIP="127.0.0.1";
+    public static final String serverIP="192.168.43.15";
     public static final int serverPort=8080;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            Socket socket=new Socket(serverIP,serverPort);
-            DataOutputStream DOS= new DataOutputStream(
-                    socket.getOutputStream());
-            DOS.writeUTF("harsimar");
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        sendData();
+    }
+    public void sendData (){
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Log.d("harsimarSingh","runnable Executing");
+                    Socket socket=new Socket(serverIP,serverPort);
+                    DataOutputStream DOS= new DataOutputStream(
+                            socket.getOutputStream());
+                    DOS.writeUTF("harsimar");
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
