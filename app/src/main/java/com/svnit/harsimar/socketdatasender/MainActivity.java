9a426f4b0de3996a3e40464ed4211a5d499a7837
Harsimar <simarsingh24@gmail.com>
2017-05-11 19:36:50 +0530

    client code added

--- app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
@@ -3,11 +3,28 @@ package com.svnit.harsimar.socketdatasender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String serverIP="127.0.0.1";
+    public static final int serverPort=8080;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Socket socket=new Socket(serverIP,serverPort);
+            DataOutputStream DOS= new DataOutputStream(
+                    socket.getOutputStream());
+            DOS.writeUTF("harsimar");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
