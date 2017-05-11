6e0f7940d1c095159d077eef0908c5fa8d7d77ee
Harsimar <simarsingh24@gmail.com>
2017-05-11 22:24:59 +0530

    bug fixes

--- app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/socketdatasender/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         final ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
         listView.setAdapter(adapter);
-
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
